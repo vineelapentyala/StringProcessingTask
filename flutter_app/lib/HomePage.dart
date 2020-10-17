15e6604f8ad0bd74668646306e8b5e55f1589d84
vineelapentyala <pentyala@seas.upenn.edu>
2020-10-17 19:43:38 -0400

    basic functionality added

---
+++ flutter_app/lib/HomePage.dart
@@ -0,0 +1,31 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutterapp/TextViewPage.dart';
+
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Center(
+//           child: Text("Word Definitions Demo", textAlign: TextAlign.center)
+//         ),
+//       ),
+//       body: Center(
+//         child:Builder(
+//             builder: (context) => Column(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: <Widget>[
+//                     RaisedButton(
+//                       color: Colors.amber,
+//                       child: Text("Open Text"),
+//                       onPressed: () {
+//                         TextViewPage();
+//                       },
+//                     ),
+//             ),
+//             ),
+//             ),
+//       ),
+//       );
+//   }
+// }
--- flutter_app/lib/TextViewPage.dart
+++ flutter_app/lib/TextViewPage.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterapp/AlertDialogView.dart';
+// import 'package:flutterapp/AlertDialogView.dart';
+import 'dart:async' show Future;
+import 'package:flutter/services.dart' show rootBundle;
 
 class TextViewPage extends StatefulWidget {
   @override
@@ -11,7 +15,6 @@ class _TextViewPageState extends State<TextViewPage> {
   @override
   Widget build(BuildContext context) {
     String fullText = 'The researchers found that word recall was greatest when the participants read aloud to themselves.\n\n"This study confirms that learning and memory benefit from active involvement," says study author Colin M. MacLeod, a professor and chair of the Department of Psychology at the University of Waterloo.';
-    String smallText = "The Researchers";
     final textSpans = <TextSpan>[];
     fullText.splitMapJoin(
       RegExp('\\w+'),
@@ -28,7 +31,8 @@ class _TextViewPageState extends State<TextViewPage> {
         return string;
       },
     );
-    return Scaffold(appBar: AppBar(title:Text("Word Definitions Demo")),
+    return Scaffold(appBar: AppBar(
+      title:Text("Word Definitions Demo")),
       body:Center(
         child: SelectableText.rich(
           TextSpan(
@@ -43,45 +47,83 @@ class _TextViewPageState extends State<TextViewPage> {
         ),
       )
     );
-    // return Scaffold(
-    //   appBar: AppBar(title: Text(" Word Definitions Demo")),
-    //   body:Center(
-    //       child: GestureDetector(
-    //                   onDoubleTap:()=>showMyDialog(context,smallText),
-    //                   child: Card(
-    //                     child: Padding(
-    //                       padding: const EdgeInsets.all(10.0),
-    //                       child: Text(smallText)
-    //                     )
-    //                   )
-    //                 //   child: SelectableText.rich(
-    //                 //   TextSpan(),
-    //                 //   style:TextStyle(
-    //                 //     color: Colors.grey[600],
-    //                 //     fontSize: 20,
-    //                 //     decoration: TextDecoration.none
-    //                 //   ),
-    //                 //   cursorColor: Colors.green,
-    //                 //   showCursor:true,
-    //                 //   toolbarOptions: ToolbarOptions(
-    //                 //     copy: true,
-    //                 //     selectAll: false,
-    //                 //     cut: false,
-    //                 //     paste: false
-    //                 //   ),
-    //                 // ),
-    //               )
-    //             )
-    //   );
   }
-  Future<void> showMyDialog(BuildContext context, String displayText) {
+  
+  Future<void> showMyDialog(BuildContext context, String displayText)async {
+    //check if you can load something just once everytime the user opens?
+    Map<dynamic,dynamic> response = jsonDecode(await rootBundle.loadString('wordDef.json'));
+    print(response);
+    // String plural;
+    String def;
+    String example;
+    if (response.containsKey(displayText)){
+      var thisWord = response[displayText];
+      def = thisWord['definition'];
+      example = thisWord['example'];
+    }
+    else{
+      def = "Not Defined";
+      example = "Not Defined";
+    }
     return showDialog<void>(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          content: Text(displayText),
+          title: Text(displayText,
+          style: TextStyle(fontWeight: FontWeight.w300)),
+          shape: RoundedRectangleBorder(
+            borderRadius:BorderRadius.all(Radius.circular(10.0))
+          ),
+          content:Builder(
+            builder:(context){
+              return Container(
+                child:Column(
+                  children: [RichText(
+                    text: TextSpan(
+                      text: "Definition: ",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold
+                        ),
+                      children:[TextSpan(
+                        text: def,
+                        style:TextStyle(
+                          fontWeight: FontWeight.w400
+                        )
+                        )
+                      ]
+                    ),
+                  ),
+                  SizedBox(
+                      height: 20,
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      text: "Example: ",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold
+                        ),
+                      children:[TextSpan(
+                        text: example,
+                        style:TextStyle(
+                          fontWeight: FontWeight.w400
+                        )
+                        )
+                      ]
+                    ),
+                  ),]
+                ),
+                height: 200,
+                width: 200
+              );
+            }
+            
+          ),
           actions: [
-            CloseButton()
+            CloseButton(
+              color: Colors.red,
+            ), 
           ]
         );
       },
